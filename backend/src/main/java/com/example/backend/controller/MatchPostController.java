438ab76db03c80937cd14d371289025ec37d6c9d
Pham Viet Duc <ducp07052@gmail.com>
2026-03-29 23:48:51 +0700

    feat: create,get matchposts

---
+++ backend/src/main/java/com/example/backend/controller/MatchPostController.java
@@ -0,0 +1,30 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.request.MatchPostCreateRequest;
+import com.example.backend.dto.response.MatchPostResponse;
+import com.example.backend.entity.Enums;
+import com.example.backend.service.MatchPostService;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/match-posts")
+public class MatchPostController {
+
+    private final MatchPostService matchPostService;
+
+    public MatchPostController(MatchPostService matchPostService) {
+        this.matchPostService = matchPostService;
+    }
+    @GetMapping
+    public ResponseEntity<Object> getMatchPosts(@RequestParam(required = false) Enums.TeamLevel skillLevel,
+                                                @RequestParam(required = false) Enums.PostType postType) {
+        return ResponseEntity.ok(matchPostService.getMatchPosts(skillLevel, postType));
+    }
+    @PostMapping
+    public ResponseEntity<MatchPostResponse> createMatchPost(@RequestBody MatchPostCreateRequest request) {
+        MatchPostResponse response = matchPostService.createMatchPost(request);
+        return ResponseEntity.ok(response);
+    }
+}
